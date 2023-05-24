01eefffb7763f451942dcd1798550628540ad162
Tagallas <100998992+Tagallas@users.noreply.github.com>
2023-05-24 20:21:12 +0200

    Update to kivyMD

--- layouts/to_do.py
+++ layouts/to_do.py
@@ -6,140 +6,77 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.checkbox import CheckBox
 
-import sqlite3
-
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.textinput import TextInput
 
+import sqlite3
+
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.list import MDList, OneLineIconListItem, IconLeftWidget, IconRightWidget, OneLineRightIconListItem, \
+    ILeftBody, ILeftBodyTouch
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.navigationdrawer import MDNavigationLayout, MDNavigationDrawerMenu, MDNavigationDrawerHeader, \
+    MDNavigationDrawerLabel, MDNavigationDrawer, MDNavigationDrawerDivider, MDNavigationDrawerItem
+
 menu_button_ratio = 8
-side_menu_button_ratio = 10
+side_menu_button_ratio = 15
 m_size_global = Window.size[0] / menu_button_ratio
 s_butt_size_global = Window.size[1] / side_menu_button_ratio
+TAG = 'ERROR'
 
-
-class ToDoList(RelativeLayout):
-    s_menu = None
-    m_size = m_size_global
-
+class ToDoList(MDNavigationLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.add_widget(ToDoListHeader())
         database = sqlite3.connect('databases/to_do.db')
         db = database.cursor()
         db.execute("SELECT label_name FROM Labels WHERE id_default=1")
-        items = db.fetchone()
+        self.items = db.fetchone()
         database.close()
-        self.display_main_screen(items[0])
+        global TAG
+        TAG = self.items[0]
 
-    def display_main_screen(self, label_name):
-        if self.s_menu:
-            self.on_menu_exit()
-        # top = height-m_size
-        self.add_widget(ToDoListScroll(label_name))
-
-    def on_menu_click(self):
-        self.s_menu = SideMenu()
-        self.add_widget(self.s_menu)
-
-    def on_menu_exit(self):
-        self.remove_widget(self.s_menu)
+    def get_tag(self):
+        return self.tag
 
 
-class ToDoListMainScreen(BoxLayout):
-    def __init__(self, text, **kwargs):
-        super().__init__(**kwargs)
-        database = sqlite3.connect('databases/to_do.db')
-        db = database.cursor()
-        db.execute("SELECT rowid FROM Labels WHERE label_name='{}'".format(text))
-        label_id = db.fetchone()[0]
-        # database.commit()
-        db.execute("""SELECT N.deadline, N.priority,  N.note, N.image, N.active
-                    FROM Labels LEFT JOIN Notes AS N on Labels.ROWID = N.label_id 
-                    WHERE N.label_id = {} AND N.active=0
-                    ORDER BY N.active, N.priority, N.deadline
-                    """.format(label_id))
-        items = db.fetchall()
-        for it in items:
-            dictionary = {'deadline': it[0], 'priority': it[1], 'note': it[2], 'image': it[3],
-                          'is_active': it[4]}
-            self.add_widget(CheckNote(dictionary))
-        self.add_widget(AddNote())
-        database.close()
-
-    def on_checkpoint(self, v):
-        print('checkpoint')
-
-
-class ToDoListScroll(ScrollView):
-    size_hint = 1, None
-    height = NumericProperty(Window.size[1] - m_size_global * 1.1)
-
-    def __init__(self, text, **kwargs):
-        super().__init__(**kwargs)
-        self.add_widget(ToDoListMainScreen(text))
-
-
-class ToDoListHeader(BoxLayout):
-    m_size = m_size_global
-
-
-class SideMenu(RelativeLayout):
-    s_butt_size = s_butt_size_global
-
-
-class SideMenuButtons(StackLayout):
-    s_butt_size = s_butt_size_global
-
+class CustomNavigationDrawer(MDList):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.add_widget(SideMenuHeader())
-        self.add_widget(Label(text='To Do', size_hint=(1, None), height=self.s_butt_size))
+        global TAG
+        self.add_widget(MDNavigationDrawerHeader(title=TAG, padding=(0, 0, 0, 10)))  # source można dodać
+        self.add_widget(MDNavigationDrawerDivider())
+        self.add_widget(OneLineIconListItem(IconLeftWidget(icon="bell", on_release=self.print, text='bell'),
+                                            text="Notifications", text_color=(.7, .7, .7, .5),
+                                            on_release=self.print, divider=None))
+        self.add_widget(MDNavigationDrawerDivider())
+        #self.add_widget(MDNavigationDrawerLabel(text="Labels"))
+        self.add_widget(OneLineRightIconListItem(IconRightWidget(icon="playlist-edit", on_release=self.print, text='edit',),
+                                            text="Labels", on_release=self.print, divider=None))
 
         database = sqlite3.connect('databases/to_do.db')
         db = database.cursor()
-        db.execute("SELECT rowid, label_name FROM Labels ORDER BY priority, label_name")
+        db.execute("SELECT label_name FROM Labels WHERE id_default=0")
         items = db.fetchall()
-        for it in items:
-            self.add_widget(Button(text=it[1], size_hint=(1, None), height=self.s_butt_size,
-                                   on_press=self.change_label))
-        database.commit()
         database.close()
-
-    def change_label(self, button):
-        print('zmiana na rowid = '+button.text)
-
-
-class SideMenuHeader(BoxLayout):
-    m_size = m_size_global
-
-
-class CheckNote(BoxLayout):
-    m_size = m_size_global
-
-    def __init__(self, items, **kwargs):
-        super().__init__(**kwargs)
-        self.add_widget(Label(size_hint=(.05, 1)))
-        self.add_widget(CheckBox(active=items['is_active'], size_hint=(.1, 1)))
-        self.add_widget(Label(size_hint=(.05, 1)))
-        self.add_widget(TextInput(size_hint=(1, 1), text=items['note'], multiline=False,
-                                  foreground_color=(1, 1, 1, 1), background_color=(0, 0, 0, 1),
-                                  halign='left', font_size=self.height/2.7, padding_y=(self.height/4.05, 0),
-                                  cursor_color=(1, 1, 1, 1)))
-        # self.add_widget(Button(text=items['note'], size_hint=(.5, 1)))
+        for el in items:
+            self.add_widget(OneLineIconListItem(IconLeftWidget(icon="label-outline", on_release=self.print, text='label'),
+                                            text=el[0], text_color=(.7, .7, .7, .5),
+                                               on_release=self.print, divider=None))
+        self.add_widget(OneLineIconListItem(IconLeftWidget(icon="plus", on_release=self.print, text='add'),
+                                            text='Add Label', text_color=(.5, .5, .5, .5),
+                                            on_release=self.print, divider=None))
+        self.add_widget(MDNavigationDrawerDivider())
+        self.add_widget(OneLineIconListItem(IconLeftWidget(icon="cog", on_release=self.print, text='cog'),
+                                            text='Settings', text_color=(.7, .7, .7, .5),
+                                            on_release=self.print, divider=None))
+        self.add_widget(OneLineIconListItem(IconLeftWidget(icon="trash-can-outline", on_release=self.print, text='trash-can'),
+                                            text='Trash', text_color=(.7, .7, .7, .5),
+                                            on_release=self.print, divider=None))
+        self.add_widget(OneLineIconListItem(IconLeftWidget(icon="help-circle-outline", on_release=self.print, text='help'),
+                                            text='Help', text_color=(.7, .7, .7, .5),
+                                            on_release=self.print, divider=None))
 
 
-class AddNote(BoxLayout):
-    m_size = m_size_global
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.add_widget(Label(size_hint=(.2, 1)))
-        self.add_widget(Button(background_normal='images/add_sign.png', size_hint=(None, 1), width=self.m_size,
-                               on_press=self.add_note, background_down='images/black_screen.png'))
-        self.add_widget(Button(text='Add Note', font_size=self.height/2.7, size_hint=(.4, 1),
-                               background_color=(0, 0, 0, 1), on_press=self.add_note))
-        self.add_widget(Label())
-
-    def add_note(self, button):
-        print('note added')
+    def print(self, butt):
+        print(butt.text)
--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from kivy import Config
-
 Config.set('graphics', 'width', '350')
 Config.set('graphics', 'height', '700')
-from kivy.app import App
+
+from kivy.properties import BooleanProperty
+from kivymd.uix.screen import MDScreen
+
+from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.metrics import dp
 from kivy.uix.button import Button
-from kivy.uix.relativelayout import RelativeLayout
+# from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.stacklayout import StackLayout
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition, FallOutTransition
+from kivy.uix.screenmanager import Screen, FadeTransition
+
+# import kivymd.app as app
+from kivymd.app import MDApp
+from kivymd.uix.screenmanager import MDScreenManager, ScreenManager
+from kivymd.uix.button import MDFlatButton, MDFillRoundFlatButton, BaseButton, MDRaisedButton
+
+# todo zastanów sie które transition
+from kivymd.uix.transition import MDFadeSlideTransition
+
 from layouts.to_do import ToDoList
 from layouts.notebook import Notebook
 from layouts.gym import Gym
@@ -19,61 +31,53 @@ Builder.load_file("kv_screens/notebook.kv")
 Builder.load_file("kv_screens/gym.kv")
 Builder.load_file("kv_screens/Multi.kv")
 
-# Clock.max_iteration = 100
 APP = 0
+window_width, window_height = Window.size
 
 
 class MainLayout(StackLayout):
-    menu_buttons = []
+    # menu_buttons = []
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         butt_size = (.5, .2)
-        self.padding = (dp(25), dp(25), dp(25), dp(25))
-        self.spacing = dp(10)
-        self.menu_buttons.append(Button(text="TO_DO", size_hint=butt_size, background_color=(.2, .2, 1, .85),
-                                        font_size=23, on_press=self.on_press))
-        self.menu_buttons.append(Button(text="Notebook", size_hint=butt_size, background_color=(.2, .2, 1, .85),
-                                        font_size=23, on_press=self.on_press))
-        self.menu_buttons.append(Button(text="GYM", size_hint=butt_size, background_color=(.2, .2, 1, .85),
-                                        font_size=23, on_press=self.on_press))
-        for i in self.menu_buttons:
-            self.add_widget(i)
+        self.padding = window_width * .07
+        self.spacing = window_width * .05
+        self.add_widget(MDRaisedButton(text="TO_DO", font_size=23,
+                                        size_hint=butt_size, md_bg_color=(.1, .2, .6, .85),
+                                        rounded_button=False, _radius=25,
+                                        on_release=self.on_press))
+        self.add_widget(MDRaisedButton(text="Notebook", font_size=23,
+                                        size_hint=butt_size, md_bg_color=(.1, .2, .6, .85),
+                                        rounded_button=False, _radius=25,
+                                        on_release=self.on_press))
+        self.add_widget(MDRaisedButton(text="GYM", font_size=23,
+                                        size_hint=butt_size, md_bg_color=(.1, .2, .6, .85),
+                                        rounded_button=False, _radius=25,
+                                        on_release=self.on_press))
 
     def on_press(self, button):
-        self.parent.parent.on_press_menu(button.text) # można options dać
-
-    # def on_press_to_do(self, button):
-    #     self.parent.parent.on_press_menu('s2') # można options dać
-    #     # if self.parent.main_menu.opacity == 1:
-    #     #     # print('todo')
-    #     #     global APP
-    #     #     APP = ToDoList()
-    #     #     self.parent.main_menu.opacity = 0
-    #     #     self.parent.update_layout()
-    #
-    # def on_press_notebook(self, button):
-    #     self.parent.parent.on_press_menu('s3') # można options dać
-    #     # if self.parent.main_menu.opacity == 1:
-    #     #     # print('notebook')
-    #     #     global APP
-    #     #     APP = Notebook()
-    #     #     self.parent.main_menu.opacity = 0
-    #     #     self.parent.update_layout()
-    #
-    # def on_press_gym(self, button):
-    #     self.parent.parent.on_press_menu('s4') # można options dać
-    #     # if self.parent.main_menu.opacity == 1:
-    #     #     # print('gym')
-    #     #     global APP
-    #     #     APP = Gym()
-    #     #     self.parent.main_menu.opacity = 0
-    #     #     self.parent.update_layout()
+        self.parent.parent.on_press_menu(button.text)
 
 
 class SM(ScreenManager):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.transition = FadeTransition()
+        self.screens = [S1(), S2(), S3(), S4()]
+        self.current = 'TO_DO'
+        # self.current = 's1'
+        # print(self.screens)
+
+    def on_press_menu(self, screen_name='s1'):
+        self.current = screen_name
+        # print(self.screens)
+
+
+class SM2(ScreenManager):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.transition = FadeTransition()
         self.screens = [S1(), S2(), S3(), S4()]
         self.current = 'TO_DO'
         #self.current = 's1'
@@ -84,42 +88,29 @@ class SM(ScreenManager):
         # print(self.screens)
 
 
-class S1(Screen):
+class S1(MDScreen):
     pass
 
 
-class S2(Screen):
+class S2(MDScreen):
     pass
 
 
-class S3(Screen):
+class S3(MDScreen):
     pass
 
 
-class S4(Screen):
+class S4(MDScreen):
     pass
 
 
-# class Menu(RelativeLayout):
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs)
-#         self.main_menu = MainLayout()
-#         self.add_widget(self.main_menu)
-#
-#     def update_layout(self):
-#         global APP
-#         secondary_menu = APP
-#         self.add_widget(secondary_menu)
-#
-#     def on_press_menu(self):
-#         # print('menu')
-#         self.main_menu.opacity = 1
-#         self.remove_widget(APP)
-
-
-class MyApp(App):
-    pass
-
+class MyApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = 'Indigo'
+        self.theme_cls.primary_hue = '900'
+        #primary hue
+        return Builder.load_file('My.kv')
 
 if __name__ == '__main__':
     MyApp().run()
